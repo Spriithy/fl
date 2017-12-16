f61d8fabccd1c1a93e29b4f8da886fb7f8156606
Spriithy <theo.dano@gmail.com>
2017-12-16 01:59:45 +0100

    virt: Added Structure types, Unit type, TypeAlias-es, exported common fields for types, better FunctionType string generation, nil-check

--- comp/types.go
+++ comp/types.go
@@ -31,9 +31,18 @@ type VoidType struct{}
 // UndefinedType represents the type of undefined values/symbols
 type UndefinedType struct{}
 
+// TypeAlias is simply used to create type aliases
+type TypeAlias struct {
+	// The name of the alias
+	Name string
+
+	// The aliased type
+	Of Type
+}
+
 // BaseType represents primitive types such comp integers and floats
 type BaseType struct {
-	alias string
+	Alias string
 
 	// Which base type (hear primitive) is concerned
 	Which int
@@ -41,7 +50,7 @@ type BaseType struct {
 
 // PointerType represents all types that are pointers to other types
 type PointerType struct {
-	alias string
+	Alias string
 
 	// The Type pointed to by the PointerType
 	To Type
@@ -49,15 +58,25 @@ type PointerType struct {
 
 // ArrayType represents all types of fixed-length
 type ArrayType struct {
-	alias string
-	size  int
+	Alias     string
+	ElemCount int
 
 	// The type of the elements held by such an array
 	Of Type
 }
 
-// A Signature is the underlying representation of a function type
-type Signature struct {
+// A StructType represents struct-like data structure
+type StructType struct {
+	Alias string
+
+	// The Fields listed in the struct type
+	Fields []Type
+}
+
+// A FunctionType holds the signature of a function
+type FunctionType struct {
+	Alias string
+
 	// The return type of a function type
 	Returns Type
 
@@ -65,21 +84,15 @@ type Signature struct {
 	Args []Type
 }
 
-// A FunctionType holds the signature of a function
-type FunctionType struct {
-	alias string
-
-	// The Signature of the function (types)
-	Signature *Signature
-}
-
 // Compile-time check
 var (
 	_ Type = (*VoidType)(nil)
 	_ Type = (*UndefinedType)(nil)
+	_ Type = (*TypeAlias)(nil)
 	_ Type = (*BaseType)(nil)
 	_ Type = (*PointerType)(nil)
 	_ Type = (*ArrayType)(nil)
+	_ Type = (*StructType)(nil)
 	_ Type = (*FunctionType)(nil)
 )
 
@@ -92,21 +105,29 @@ const (
 	BasicKind
 	PointerKind
 	ArrayKind
+	StructKind
 	FunctionKind
 )
 
 // These values are used to identify builtin types
 const (
-	u8typeId = iota
-	u16typeId
-	u32typeId
-	u64typeId
-	i8typeId
-	i16typeId
-	i32typeId
-	i64typeId
-	f64typeId
-	f32typeId
+	u8typeID = iota
+	u16typeID
+	u32typeID
+	u64typeID
+	i8typeID
+	i16typeID
+	i32typeID
+	i64typeID
+	f64typeID
+	f32typeID
+)
+
+const (
+	voidTypeString      = "void"
+	undefinedTypeString = "undef"
+	funcTypeArrow       = "->"
+	nilTypeReplacement  = "%!(nil)"
 )
 
 /******************************************************************************/
@@ -118,7 +139,7 @@ func (t *VoidType) Size() int {
 }
 
 func (t *VoidType) TypeString() string {
-	return t.String()
+	return voidTypeString
 }
 
 func (t *VoidType) Kind() TypeKind {
@@ -126,11 +147,64 @@ func (t *VoidType) Kind() TypeKind {
 }
 
 func (t *VoidType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
 	return other.Kind() == VoidKind
 }
 
 func (t *VoidType) String() string {
-	return ""
+	return voidTypeString
+}
+
+/******************************************************************************/
+/*** TypeAlias implementation                                               ***/
+/******************************************************************************/
+
+func (t *TypeAlias) Size() int {
+	if t.Of == nil {
+		return 0
+	}
+
+	return t.Of.Size()
+}
+
+func (t *TypeAlias) TypeString() string {
+	switch t.Name {
+	case "":
+		if t.Of == nil {
+			return nilTypeReplacement
+		}
+
+		return t.Of.String()
+	default:
+		return t.Name
+	}
+}
+
+func (t *TypeAlias) Kind() TypeKind {
+	if t.Of == nil {
+		return -1
+	}
+
+	return t.Of.Kind()
+}
+
+func (t *TypeAlias) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
+	if t.Of == nil {
+		return false
+	}
+
+	return t.Of.Equals(other)
+}
+
+func (t *TypeAlias) String() string {
+	return t.TypeString()
 }
 
 /******************************************************************************/
@@ -142,7 +216,7 @@ func (t *UndefinedType) Size() int {
 }
 
 func (t *UndefinedType) TypeString() string {
-	return t.String()
+	return undefinedTypeString
 }
 
 func (t *UndefinedType) Kind() TypeKind {
@@ -150,11 +224,15 @@ func (t *UndefinedType) Kind() TypeKind {
 }
 
 func (t *UndefinedType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
 	return other.Kind() == UndefinedKind
 }
 
 func (t *UndefinedType) String() string {
-	return "undefined"
+	return undefinedTypeString
 }
 
 /******************************************************************************/
@@ -163,13 +241,13 @@ func (t *UndefinedType) String() string {
 
 func (t *BaseType) Size() int {
 	switch t.Which {
-	case i8typeId, u8typeId:
+	case i8typeID, u8typeID:
 		return 1
-	case i16typeId, u16typeId:
+	case i16typeID, u16typeID:
 		return 2
-	case i32typeId, u32typeId, f32typeId:
+	case i32typeID, u32typeID, f32typeID:
 		return 4
-	case i64typeId, u64typeId, f64typeId:
+	case i64typeID, u64typeID, f64typeID:
 		return 8
 	}
 
@@ -185,6 +263,10 @@ func (t *BaseType) Kind() TypeKind {
 }
 
 func (t *BaseType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
 	if other.Kind() != BasicKind {
 		return false
 	}
@@ -193,8 +275,8 @@ func (t *BaseType) Equals(other Type) bool {
 }
 
 func (t *BaseType) String() string {
-	if t.alias != "" {
-		return t.alias
+	if t.Alias != "" {
+		return t.Alias
 	}
 
 	return t.TypeString()
@@ -207,7 +289,12 @@ func (t *BaseType) String() string {
 func (t *PointerType) Size() int {
 	return int(unsafe.Sizeof(uintptr(0)))
 }
+
 func (t *PointerType) TypeString() string {
+	if t.To == nil {
+		return nilTypeReplacement + "&"
+	}
+
 	return t.To.String() + "&"
 }
 
@@ -216,6 +303,10 @@ func (t *PointerType) Kind() TypeKind {
 }
 
 func (t *PointerType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
 	if other.Kind() != PointerKind {
 		return false
 	}
@@ -224,8 +315,8 @@ func (t *PointerType) Equals(other Type) bool {
 }
 
 func (t *PointerType) String() string {
-	if t.alias != "" {
-		return t.alias
+	if t.Alias != "" {
+		return t.Alias
 	}
 
 	return t.TypeString()
@@ -236,11 +327,19 @@ func (t *PointerType) String() string {
 /******************************************************************************/
 
 func (t *ArrayType) Size() int {
-	return t.size * t.Of.Size()
+	if t.Of == nil {
+		return 0
+	}
+
+	return t.ElemCount * t.Of.Size()
 }
 
 func (t *ArrayType) TypeString() string {
-	return fmt.Sprintf("[%d]%s", t.size, t.Of.String())
+	if t.Of == nil {
+		return fmt.Sprintf("%s[%d]", nilTypeReplacement, t.ElemCount)
+	}
+
+	return fmt.Sprintf("%s[%d]", t.Of.String(), t.ElemCount)
 }
 
 func (t *ArrayType) Kind() TypeKind {
@@ -248,6 +347,14 @@ func (t *ArrayType) Kind() TypeKind {
 }
 
 func (t *ArrayType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
+	if other == nil {
+		return false
+	}
+
 	if other.Kind() != ArrayKind {
 		return false
 	}
@@ -256,8 +363,72 @@ func (t *ArrayType) Equals(other Type) bool {
 }
 
 func (t *ArrayType) String() string {
-	if t.alias != "" {
-		return t.alias
+	if t.Alias != "" {
+		return t.Alias
+	}
+
+	return t.TypeString()
+}
+
+/******************************************************************************/
+/*** StructType implementation                                              ***/
+/******************************************************************************/
+
+func (t *StructType) Size() int {
+	if t.Fields == nil {
+		return 0
+	}
+
+	var size int
+	for _, typ := range t.Fields {
+		if typ != nil {
+			size += typ.Size()
+		}
+	}
+
+	return size
+}
+
+func (t *StructType) TypeString() (repr string) {
+	if t.Fields == nil || len(t.Fields) == 0 {
+		return "struct{}"
+	}
+
+	repr += "struct {\n  "
+	for _, typ := range t.Fields {
+		if typ == nil {
+			repr += nilTypeReplacement
+		} else {
+			repr += typ.String()
+		}
+		repr += "\n  "
+	}
+
+	// Remove the leading spaces on the last type line
+	repr = repr[:len(repr)-2]
+
+	return repr + "}"
+}
+
+func (t *StructType) Kind() TypeKind {
+	return StructKind
+}
+
+func (t *StructType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
+	if other.Kind() != StructKind {
+		return false
+	}
+
+	return t.TypeString() == other.TypeString()
+}
+
+func (t *StructType) String() string {
+	if t.Alias != "" {
+		return "struct " + t.Alias
 	}
 
 	return t.TypeString()
@@ -267,28 +438,57 @@ func (t *ArrayType) String() string {
 /*** FunctionType implementation                                            ***/
 /******************************************************************************/
 
+// DefaultFunctionReturnValue is used to determine the return type of functions
+// that would miss such annotation
+var DefaultFunctionReturnValue = Unit
+
 func (t *FunctionType) Size() int {
 	return int(unsafe.Sizeof(uintptr(0))) // size of pointer
 }
 
-func (t *FunctionType) TypeString() string {
-	if t.Signature == nil {
-		return "func()"
+func (t *FunctionType) TypeString() (repr string) {
+	if t.Args == nil {
+		repr = "()"
+
+		if t.Returns == nil {
+			repr += " " + funcTypeArrow + " " + nilTypeReplacement
+			return repr
+		} else if len(t.Returns.String()) == 0 {
+			repr += " " + funcTypeArrow + " " + DefaultFunctionReturnValue.String()
+			return repr
+		}
+
+		repr += " " + funcTypeArrow + " " + t.Returns.String()
+		return strings.TrimSpace(repr)
 	}
 
-	repr := "func("
-	for _, typ := range t.Signature.Args {
-		repr += typ.String()
+	repr = "("
+	for _, typ := range t.Args {
+		if typ != nil {
+			repr += typ.String()
+		} else {
+			repr += nilTypeReplacement
+		}
 		repr += ", "
 	}
 
 	// Cut the trailing coma+space if there were args
-	if len(t.Signature.Args) > 0 {
+	if len(t.Args) > 0 {
 		repr = repr[:len(repr)-2]
 	}
 
 	repr += ") "
-	repr += t.Signature.Returns.String()
+	if t.Returns != nil {
+		// Add arrow only if type actually has a type string
+		if len(t.Returns.String()) > 0 {
+			repr += funcTypeArrow + " "
+		}
+
+		repr += t.Returns.String()
+	} else {
+		repr += funcTypeArrow + " "
+		repr += DefaultFunctionReturnValue.String()
+	}
 
 	return strings.TrimSpace(repr)
 }
@@ -298,6 +498,10 @@ func (t *FunctionType) Kind() TypeKind {
 }
 
 func (t *FunctionType) Equals(other Type) bool {
+	if other == nil {
+		return false
+	}
+
 	if other.Kind() != FunctionKind {
 		return false
 	}
@@ -306,8 +510,8 @@ func (t *FunctionType) Equals(other Type) bool {
 }
 
 func (t *FunctionType) String() string {
-	if t.alias != "" {
-		return t.alias
+	if t.Alias != "" {
+		return t.Alias
 	}
 
 	return t.TypeString()
